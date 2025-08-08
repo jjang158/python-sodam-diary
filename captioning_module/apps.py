cb8f574343b7651f3edd12cce56af739241372e2
eyjang <sa358@naver.com>
2025-08-08 16:47:20 +0900

    Mac 호환성 개선

--- captioning_module/apps.py
+++ captioning_module/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-from captioning_module.model.model_loader import ModelLoader
+from captioning_module.model.model_loader import ModelLoader, ModelLoader_mac
 
 class CaptioningModuleConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "captioning_module"
     
     def ready(self):
-        ModelLoader.get_clip()
-        ModelLoader.get_blip()
+        ModelLoader_mac.get_clip()
+        ModelLoader_mac.get_blip()
