85656bc1582db34868150c8c4c6057e39db55a18
Hong Seung Wan <hong@hongui-MacBookAir.local>
2025-11-16 14:15:47 +0900

    feat: Finalize cleanup by removing all Django legacy files

--- captioning_module/admin.py
+++
@@ -1,6 +0,0 @@
-# captioning_module/admin.py
-
-from django.contrib import admin
-from .models import Image
-
-admin.site.register(Image)
--- captioning_module/serializers.py
+++
@@ -1,9 +0,0 @@
-# captioning_module/serializers.py
-
-from rest_framework import serializers
-from .models import Image
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Image
-        fields = '__all__'
