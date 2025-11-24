1091279a173e78ac96d07b108b5f005d5035962b
eyjang <sa358@naver.com>
2025-11-24 22:03:04 +0900

    openvino 버전 변경(2024.2.0 -> 2025.3.0) openvino==2025.3.0

--- captioning_module/model_config.py
+++ captioning_module/model_config.py
@@ -7,7 +7,7 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 BLIP_MODEL_DIR = os.path.join(
     FILE_DIR, 
     # "blip_openvino_base",
-    "blip_openvino_large",
+    "blip_openvino",
 )
 BLIP_MODEL_PATH = os.path.join(
     BLIP_MODEL_DIR, "blip_caption.xml"
